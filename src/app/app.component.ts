4985a5100c49a014d2874fcabb01c3d5c923fec5
adsgabriel <63120824+adsgabriel@users.noreply.github.com>
2022-03-15 22:54:55 -0300

    add metodos e validações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +9,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  public todos: any[] = [];
+  public todos: Todo[] = [];
   public title: String = 'Minhas Tarefas';
+  public form: FormGroup;
 
-  constructor() {
-      this.todos.push('estudar angular');
-      this.todos.push('estudar Vue');
-      this.todos.push('estudar Node');
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required,
+      ])]
+    });
+      this.todos.push(new Todo(1, 'Passear com o cachorro', false));
+      this.todos.push(new Todo(2, 'Lavar o Carro', true));
+      this.todos.push(new Todo(3, 'Estudar Angular', true));
+  }
 
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1){
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
   }
 }
