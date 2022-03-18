fe4b0beb382c5353bf6abc248c122c7dbfb6e34a
adsgabriel <63120824+adsgabriel@users.noreply.github.com>
2022-03-17 22:53:53 -0300

    add operações do crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,23 +21,48 @@ export class AppComponent {
         Validators.required,
       ])]
     });
-      this.todos.push(new Todo(1, 'Passear com o cachorro', false));
-      this.todos.push(new Todo(2, 'Lavar o Carro', true));
-      this.todos.push(new Todo(3, 'Estudar Angular', true));
+
+    this.load();
   }
 
   remove(todo: Todo) {
     const index = this.todos.indexOf(todo);
     if (index !== -1){
       this.todos.splice(index, 1);
+      this.save();
     }
+  }
+
+  add(){
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+  }
+
+
+  clear(){
+    this.form.reset();
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
   }
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+    this.save();
+  }
+
+  save(){
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load(){
+    const data = localStorage.getItem('todos');
+    this.todos = JSON.parse(data);
   }
 }
